3cb5c745f9411e3d0ba8088847a007b63f2db1a5
Khusniyarov-Arthur <Khusniyarovaa@yandex.ru>
2023-03-05 17:26:29 +0300

    lesson-01 init webpack project

---
+++ src/module.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import coffe from './img/free.png'
+
+const Module = () => (
+  <>
+    <h1> hello, REACT!</h1>
+    <img width={250} src={coffe} />
+  </>
+)
+
+export default Module;
